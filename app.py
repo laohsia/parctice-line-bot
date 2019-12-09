9329c3ff4ca8bb4b39694cb4d36fc0de93bf8907
LaoHsia <a4890894@gmail.com>
2019-12-09 11:14:39 +0800

    update

--- app.py
+++ app.py
@@ -50,10 +50,10 @@ def handle_message(event):
         sticker_message)
         return
     
-    if msg != [] :
+    if ["找工作","職缺","工作"] in msg :
         re = "你好~很高興認識你，感謝您加職缺小幫手為您的好友!\n小幫手為您快速搜尋符合專業的職缺，請在下列選單中點選您所需要的選項。\n由左至右分別為學生、新鮮人、上班族，請依身分選擇，系統會導向對應的求職頁面!\n假如沒有符合的選項，請輸入[不符合身分]"
-        if msg == "不符合身分" or "[不符合身分]" :
-            re = "請稍等~XD我還沒更新功能哈哈哈哈"
+    if "不符合身分" in msg:
+        re = "請稍等，軟體更新功能中"
         
     line_bot_api.reply_message(
     event.reply_token,
