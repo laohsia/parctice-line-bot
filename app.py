c14dd9700f72585bdf80384ac2168d42ae55ae6f
LaoHsia <a4890894@gmail.com>
2019-12-09 15:57:21 +0800

    update

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage
+    MessageEvent, TextMessage, TextSendMessage, StickerSendMessage, TempateSendMessage
 )
 
 app = Flask(__name__)
@@ -53,7 +53,36 @@ def handle_message(event):
     elif "找工作" in msg :
         re = "你好~很高興認識你，感謝您加職缺小幫手為您的好友!\n小幫手為您快速搜尋符合專業的職缺，請在下列選單中點選您所需要的選項。\n由左至右分別為學生、新鮮人、上班族，請依身分選擇，系統會導向對應的求職頁面!\n假如沒有符合的選項，請輸入[不符合身分]"
     elif "不符合身分" in msg:
-        re = "請稍等，軟體更新功能中"
+        re = {
+  "type": "flex",
+  "altText": "Flex Message",
+  "contents": {
+    "type": "bubble",
+    "direction": "ltr",
+    "footer": {
+      "type": "box",
+      "layout": "horizontal",
+      "contents": [
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "主管職",
+            "uri": "https://www.104.com.tw/jobs/main/higher"
+          }
+        },
+        {
+          "type": "button",
+          "action": {
+            "type": "uri",
+            "label": "中高齡",
+            "uri": "https://senior.104.com.tw/?tabs=jobs&utm_source=official_104&utm_medium=104_menu_elderly"
+          }
+        }
+      ]
+    }
+  }
+}
     elif "學生" in msg:
         re = "請輸入打工性質 :  長期、短期、假日、寒假、暑假"
     elif msg in ["長期","短期","假日","寒假","暑假"]:
